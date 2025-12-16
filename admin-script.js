02952be3d50f62a241b4a663b15589f1d29c0f03
Ray Abid <ray.abid@arcelormittal.com>
2025-12-16 13:33:32 -0500

    dinner time bug fix

--- admin-script.js
+++ admin-script.js
@@ -129,7 +129,7 @@ function renderAllTable() {
             <td data-col="phone">${escapeHtml(rsvp.phone)}</td>
             <td data-col="instagram">${escapeHtml(rsvp.instagram)}</td>
             <td data-col="guests">${rsvp.guests}</td>
-            <td data-col="time">${formatDinnerTime(rsvp.dinnertime)}</td>
+            <td data-col="time">${rsvp.dinnertime ? escapeHtml(rsvp.dinnertime) : '-'}</td>
             <td data-col="notes">${rsvp.notes ? escapeHtml(rsvp.notes) : '-'}</td>
             <td data-col="timestamp">${formatTimestamp(rsvp.timestamp)}</td>
         </tr>
@@ -153,7 +153,7 @@ function renderVIPTable() {
             <td>${escapeHtml(rsvp.phone)}</td>
             <td>${escapeHtml(rsvp.instagram)}</td>
             <td>${rsvp.guests}</td>
-            <td><span class="time-slot">${formatDinnerTime(rsvp.dinnertime)}</span></td>
+            <td><span class="time-slot">${rsvp.dinnertime ? escapeHtml(rsvp.dinnertime) : '-'}</span></td>
             <td>${rsvp.notes ? escapeHtml(rsvp.notes) : '-'}</td>
             <td>${formatTimestamp(rsvp.timestamp)}</td>
         </tr>
